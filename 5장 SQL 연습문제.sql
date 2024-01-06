9b44754f23816af147723be2427e47af281155c2
gudals9677 <gudals9677@gmail.com>
2024-01-06 19:49:09 +0900

    d

--- 5장 SQL 연습문제.sql
+++ 5장 SQL 연습문제.sql
@@ -12,8 +12,8 @@ flush privileges;
 create table `Customer` (
 	`custId`	varchar(10) primary key,
     `name`		varchar(10) not null,
-    `hp`		varchar(13) unique,
-    `addr`		varchar(100) unique,
+    `hp`		varchar(13) default null unique,
+    `addr`		varchar(100) default null,
     `rdate`		date not null
 	);
 select * from `Customer`;
@@ -21,14 +21,14 @@ drop table `Customer`;
 create table `Product`(
 	`prodNo`	int primary key,
 	`prodName`	varchar(10) not null,
-    `stock`		int default '0',
-    `price`		int unique,
+    `stock`		int default 0,
+    `price`		int default null,
     `company`	varchar(20) not null
     );
 select * from `Product`;
 drop table `Product`;
 create table `Order` (
-	`orderNo`	int primary key auto_increment,
+	`orderNo`	int auto_increment primary key,
     `orderId`	varchar(10) not null,
     `orderProduct`	int not null,
     `orderCount`	int default 1,
@@ -65,96 +65,99 @@ insert into `Order` (`orderId`,`orderProduct`,`orderCount`,`orderDate`) values('
 insert into `Order` (`orderId`,`orderProduct`,`orderCount`,`orderDate`) values('c104',2,4,'2022-07-04 14:23:23');
 insert into `Order` (`orderId`,`orderProduct`,`orderCount`,`orderDate`) values('c102',1,3,'2022-07-04 21:54:34');
 insert into `Order` (`orderId`,`orderProduct`,`orderCount`,`orderDate`) values('c107',6,1,'2022-07-05 14:21:03');
-#실습 1-3
+
 #실습 1-4
 select * from `customer`;
 
 #실습 1-5
-select `custId`,`name`,`hp` from `customer`; 
-
+select  `custId`,`name`,`hp` from `customer`;
 #실습 1-6
 select * from `product`;
 
 #실습 1-7
-select `company` from `product`;
+select `company` from `product`; 
 
 #실습 1-8
-select distinct `company` from `product`;
+select  distinct `company` from `product`;
 
 #실습 1-9
-select `prodName`,`price` from `product`;
+select `prodname`,`price` from `product`;
 
 #실습 1-10
-select `prodname`,`price`+500 as `조정단가` from `product`;
+select `prodname`,`price` + 500 as `조정단가` from `product`;
 
 #실습 1-11
-select `prodName`,`stock`,`price` from `Product` where `company` = '오리온';
+select `prodname`,`stock`,`price` from `product` where `company`='오리온';
 
 #실습 1-12
-select `orderProduct`, `orderCount`,`orderDate` from `Order` where `orderId` = 'c102';
+select `orderproduct`,`orderCount`,`orderDate`
+ from `order` where `orderid`='c102';
 
 #실습 1-13
-select `orderProduct`,`orderCount`,`orderDate` from `Order` where `orderId` = 'c102' and `orderCount` >= 2;
+select `orderproduct`,`ordercount`,`orderdate` from `order`
+where `orderid`='c102' and `ordercount` >= 2;
 
 #실습 1-14
-select * from `product` where `price` >=1000 and `price` <=2000;
 select * from `product` where `price` between 1000 and 2000;
 
 #실습 1-15
-select `custid`, `name`, `hp`, `addr` from `Customer` where `name` like '김%';
+select `custid`,`name`,`hp`,`addr` from `customer` where `name` like '김%';
 
 #실습 1-16
-select `custid`, `name`, `hp`, `addr` from `Customer` where `name` like '__';
+select `custid`,`name`,`hp`,`addr` from `customer` where `name` like '__';
 
 #실습 1-17
 select * from `customer` where `hp` is null;
 
 #실습 1-18
-select * from `Customer` where `addr` != 'null';
+select * from `customer` where `addr` is not null;
 
 #실습 1-19
-select * from  `Customer` order by `rdate` desc;
+select * from `customer` order by `rdate` desc;
 
 #실습 1-20
-select * from `order` where `ordercount` >= 3 order by `ordercount` desc , `orderproduct` asc;
+select * from `order` where `ordercount` >= 3 order by `orderCount`
+desc, `orderproduct` asc;
 
 #실습 1-21
-select avg(`price`) from `product`;
+select avg(price) from `product`;
 
 #실습 1-22
-select sum(`stock`) as `재고량 합계` from `Product` where `company`='농심';
+select sum(stock) as `재고량 합계` from `product` where `company` ='농심';
 
 #실습 1-23
-select count(*) as `고객수` from `customer`;
+select count(*) as `고객 수` from `customer`;
 
 #실습 1-24
-select count(distinct `company`) as `제조업체 수 ` from `product`;
+select count(distinct company) as `제조업체 수` from `product`;
 
 #실습 1-25
 select `orderproduct` as `주문 상품번호`, sum(`ordercount`) as
- `총 주문수량` from `order` group by `orderproduct` order by `주문 상품번호`;
+`총 주문수량` from `order` group by `orderproduct` order by `주문 상품번호`;
 
 #실습 1-26
 select `company` as `제조업체`, count(*) as `제품수`, max(`price`)
- as `최고가` from `product` group by `company` order by `company`;
+as `최고가` from `product` group by `company` order by `company`asc;
 
 #실습 1-27
 select `company` as `제조업체`, count(*) as `제품수`, max(`price`)
- as `최고가` from `product` group by `company` having `제품수` >=2;
+as `최고가` from `product` group by `company` having `제품수` >=2;
 
 #실습 1-28
-select `orderproduct`, `orderid`, sum(`ordercount`)
- as `총 주문수량` from `order` group by `orderproduct`,`orderid` order by `orderproduct`;
+select `orderproduct`,`orderid`,sum(`ordercount`) as `총 주문수량`
+from `order` group by `orderproduct`,`orderid` order by `orderproduct`;
 
 #실습 1-29
-select a.orderid, b.prodname from `order`
- as a join  `product`
- as b on a.orderproduct = b.prodno where `orderid` = 'c102';
+select `orderid`,`prodname` from `order` as a join `product` as b
+on a.orderproduct=b.prodno
+where `orderid` = 'c102';
 
 #실습 1-30
-select `orderid`, `name`, `prodname`,`orderdate` from `order` as a
+select `orderid`,`name`,`prodname`,`orderdate` from `order` as a
 join `customer` as b
 on a.orderid = b.custid
 join `product` as c
 on a.orderproduct = c.prodno
 where left(`orderdate`,10) = '2022-07-03';
+
+
