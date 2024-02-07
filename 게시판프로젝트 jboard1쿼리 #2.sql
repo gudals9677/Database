e4e33f6e5042dc5ac23a62c05dda752f13c6ecd8
gudals9677 <gudals9677@gmail.com>
2024-02-07 12:41:04 +0900

    213

---
+++ 게시판프로젝트 jboard1쿼리 #2.sql
@@ -0,0 +1,18 @@
+SELECT 
+	a.*,
+	b.`nick`
+FROM `Article` AS a
+JOIN `User` AS b ON a.writer = b.uid
+ORDER BY `no` DESC
+LIMIT 0, 10;
+
+INSERT INTO `Article` (`title`,`content`,`writer`,`regip`,`rdate`)
+SELECT `title`,`content`,`writer`,`regip`,`rdate` FROM `Article`;
+
+SELECT COUNT(*) FROM `Article`;
+
+SELECT * FROM `Article` WHERE `no`= '1';
+
+DELETE FROM `Article` WHERE `no` > 5;
+
+UPDATE `Article` SET `hit` = `hit` +1 WHERE `no`=1707248;
